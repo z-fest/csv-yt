63180b358cc26d85551d572e3c06c2c15b599990
Jeron Aldaron Lau <aldaronlau@gmail.com>
2023-04-09 20:56:05 -0500

    Generate output paths from CSV file.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::env;
 
+type Result<T = (), E = ()> = std::result::Result<T, E>;
+
 #[derive(Debug)]
 enum Subcommand {
     /// Append contents of CSV column to the end of the filename
@@ -12,15 +14,16 @@ enum Subcommand {
     DirLit(String),
 }
 
-fn help() -> Result<(), ()> {
+fn help() -> Result {
     println!("That's not how you use this command");
-    println!("");
+    println!();
     println!("Look at the docs");
 
     Err(())
 }
 
-fn main() -> Result<(), ()> {
+fn main() -> Result {
+    let mut commands = Vec::new();
     let mut args = env::args().skip(1);
     let Some(csv_file) = args.next() else {
         return help();
@@ -35,7 +38,6 @@ fn main() -> Result<(), ()> {
         let Some(parameter) = args.next() else {
             return help();
         };
-
         let command = match command.as_str() {
             "fv" => Subcommand::FileVar,
             "fl" => Subcommand::FileLit,
@@ -44,7 +46,78 @@ fn main() -> Result<(), ()> {
             _ => return help(),
         }(parameter);
 
-        println!("  Command: {command:?}");
+        commands.push(command);
+    }
+
+    println!("Commands: {commands:?}");
+
+    let Ok(mut csv_reader) = csv::Reader::from_path(&csv_file) else {
+        eprintln!("Failed to open file: {csv_file}");
+
+        return Err(());
+    };
+
+    let Ok(headers) = csv_reader.headers() else {
+        eprintln!("File {csv_file} is not valid CSV");
+
+        return Err(());
+    };
+    let headers: Vec<_> = headers.iter().map(String::from).collect();
+
+    for result in csv_reader.records() {
+        let Ok(record) = result else {
+            eprintln!("File {csv_file} is not valid CSV");
+
+            return Err(());
+        };
+
+        let mut dirname = String::new();
+        let mut filename = String::new();
+
+        for cmd in commands.iter() {
+            use Subcommand::*;
+
+            match cmd {
+                FileVar(var) => {
+                    let mut lit = None;
+
+                    for (header, value) in headers.iter().zip(record.iter()) {
+                        if header == var {
+                            lit = Some(value);
+                        }
+                    }
+
+                    let Some(lit) = lit else {
+                        eprintln!("No CSV column of name {var}");
+
+                        return Err(());
+                    };
+
+                    filename.push_str(&lit);
+                },
+                FileLit(lit) => filename.push_str(&lit),
+                DirVar(var) => {
+                    let mut lit = None;
+
+                    for (header, value) in headers.iter().zip(record.iter()) {
+                        if header == var {
+                            lit = Some(value);
+                        }
+                    }
+
+                    let Some(lit) = lit else {
+                        eprintln!("No CSV column of name {var}");
+
+                        return Err(());
+                    };
+
+                    dirname.push_str(&lit);
+                },
+                DirLit(lit) => dirname.push_str(&lit),
+            }
+        }
+
+        println!("{dirname}/{filename}");
     }
 
     Ok(())
